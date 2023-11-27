77b691e8aab16b10897570a73946f0104789586a
why-1513 <1138828080@qq.com>
2023-11-27 10:22:23 +0800

    增加了文件读取

--- configuration/main.cpp
+++ configuration/main.cpp
@@ -6,9 +6,16 @@ extern void yyparse_init(const char* filename);
 extern void yyparse_cleanup();
 
 int main(int argc, char **argv) {
+  const char* config_name;
   
-  yyparse_init("PIN");
+  std::cout << "Please enter the config name: ";
+  std::string userInput;
+  std::cin >> userInput;
+
+  config_name = userInput.c_str();
+
+  yyparse_init(config_name);
   yyparse();
-  yyparse_cleanup;
+  yyparse_cleanup();
   return 0;
 }
