4b6aa8c6a6a547dd2a8b14bdb8c9befea8905505
why-1513 <1138828080@qq.com>
2024-01-03 16:19:33 +0800

    对类的名称进行重命名

--- configuration/ConfigAST.h
+++ configuration/ConfigAST.h
@@ -21,23 +21,23 @@ public:
         }
     }
 
-    class DFPN {
+    class DefinePinName {
     public:
-        std::vector<std::string> channel_no;
+        std::vector<std::string> channelNo;
         std::vector<std::string> pinNo;
         std::vector<std::string> pin;
 
-        void addDFPNData(const std::string& infoChannelNo,
+        void addData(const std::string& infoChannelNo,
          const std::string& infoPinNo, const std::string& infoPin) {
-            channel_no.push_back(infoChannelNo);
+            channelNo.push_back(infoChannelNo);
             pinNo.push_back(infoPinNo);
             pin.push_back(infoPin);
         } 
 
-        void printDFPNData() const {
-            std::cout << "DFPN Data:" << std::endl;
+        void printData() const {
+            std::cout << "DefinePinName Data:" << std::endl;
             std::cout << "Channel No: ";
-            for (const auto& channel : channel_no) {
+            for (const auto& channel : channelNo) {
                 std::cout << channel << " ";
             }
             std::cout << std::endl;
@@ -56,25 +56,25 @@ public:
         }
     };
 
-    std::shared_ptr<DFPN> dfpn = std::make_shared<DFPN>();
+    std::shared_ptr<DefinePinName> dfpn = std::make_shared<DefinePinName>();
 
-    class DFPS {
+    class DefinePowerSupply {
     public:
-        std::vector<std::string> dps_channel;
+        std::vector<std::string> dpsChannel;
         std::vector<std::string> polarity;
         std::vector<std::string> pin;
 
-        void addDFPSData(const std::string& infoDpsChannel,
+        void addData(const std::string& infoDpsChannel,
          const std::string& infoPolarity, const std::string& infoPin){
-            dps_channel.push_back(infoDpsChannel);
+            dpsChannel.push_back(infoDpsChannel);
             polarity.push_back(infoPolarity);
             pin.push_back(infoPin);
         }
 
-        void printDFPSData() const {
+        void printData() const {
             std::cout << "DFPS Data:" << std::endl;
             std::cout << "DPS Channel: ";
-            for (const auto& channel : dps_channel) {
+            for (const auto& channel : dpsChannel) {
                 std::cout << channel << " ";
             }
             std::cout << std::endl;
@@ -93,9 +93,9 @@ public:
         }
     };
 
-    std::shared_ptr<DFPS> dfps = std::make_shared<DFPS>();
+    std::shared_ptr<DefinePowerSupply> dfps = std::make_shared<DefinePowerSupply>();
 
-    class PSTE {
+    class ParallelSiteEnable {
     public:
         std::string sites;
 
@@ -103,26 +103,26 @@ public:
             sites = infoSites;
         }
 
-        void printPSTEData() const {
+        void printData() const {
             std::cout << "PSTE Data:" << std::endl;
             std::cout << "Sites: " << sites << std::endl;
         }
     };
 
-    std::shared_ptr<PSTE> pste = std::make_shared<PSTE>();
+    std::shared_ptr<ParallelSiteEnable> pste = std::make_shared<ParallelSiteEnable>();
 
-    class CONF {
+    class CONFGuration {
     public:
         std::vector<std::string> context;
-        std::vector<std::string> pin_type;
-        std::vector<std::string> pin_oper_mode;
+        std::vector<std::string> pinType;
+        std::vector<std::string> pinOperMode;
         std::vector<std::vector<std::string>> pinlist;
 
-        void addCONFData(const std::string& infoContext, const std::string& infoPinType,
+        void addData(const std::string& infoContext, const std::string& infoPinType,
          const std::string& infoPinOperMode, const std::vector<std::string>& infoPinList) {
             context.push_back(infoContext);
-            pin_type.push_back(infoPinType);
-            pin_oper_mode.push_back(infoPinOperMode);
+            pinType.push_back(infoPinType);
+            pinOperMode.push_back(infoPinOperMode);
             pinlist.push_back(infoPinList);
         }
 
@@ -136,7 +136,7 @@ public:
             }
         }
 
-        void printCONFData() const {
+        void printData() const {
             std::cout << "CONF Data:" << std::endl;
             std::cout << "Context: ";
             for (const auto& text : context) {
@@ -145,13 +145,13 @@ public:
             std::cout << std::endl;
 
             std::cout << "PinType: ";
-            for (const auto& type : pin_type) {
+            for (const auto& type : pinType) {
                 std::cout << type << " ";
             }
             std::cout << std::endl;
 
             std::cout << "PinOperMode: ";
-            for (const auto& mode : pin_oper_mode) {
+            for (const auto& mode : pinOperMode) {
                 std::cout << mode << " ";
             }
             std::cout << std::endl;
@@ -159,18 +159,18 @@ public:
 
     };
 
-    std::shared_ptr<CONF> conf = std::make_shared<CONF>();
+    std::shared_ptr<CONFGuration> conf = std::make_shared<CONFGuration>();
 
-    class DFGP {
+    class DefineGroup {
     public:
-        std::vector<std::string> pin_type;
-        std::vector<std::string> pin_group;
+        std::vector<std::string> pinType;
+        std::vector<std::string> pinGroup;
         std::vector<std::vector<std::string>> pinlist;
 
-        void addDFGPData(const std::string& infoPinType,
+        void addData(const std::string& infoPinType,
          const std::vector<std::string>& infoPinList, const std::string& infoPinGroup) {
-            pin_type.push_back(infoPinType);
-            pin_group.push_back(infoPinGroup);
+            pinType.push_back(infoPinType);
+            pinGroup.push_back(infoPinGroup);
             pinlist.push_back(infoPinList);
         }
 
@@ -184,17 +184,17 @@ public:
             }
         }
 
-        void printDFGPData() const {
-            std::cout << "CONF Data:" << std::endl;
+        void printData() const {
+            std::cout << "DFGP Data:" << std::endl;
 
             std::cout << "PinType: ";
-            for (const auto& type : pin_type) {
+            for (const auto& type : pinType) {
                 std::cout << type << " ";
             }
             std::cout << std::endl;
 
             std::cout << "PinGroup: ";
-            for (const auto& group : pin_group) {
+            for (const auto& group : pinGroup) {
                 std::cout << group << " ";
             }
             std::cout << std::endl;
@@ -202,18 +202,18 @@ public:
     
     };
 
-    std::shared_ptr<DFGP> dfgp = std::make_shared<DFGP>();
+    std::shared_ptr<DefineGroup> dfgp = std::make_shared<DefineGroup>();
 
-    class PSLC {
+    class PowerSupplyLoadCapacitance {
     public:
         std::vector<std::string> value;
-        std::vector<std::string> DCS_value;
+        std::vector<std::string> DCSValue;
         std::vector<std::vector<std::string>> pinlist;
 
-        void addPSLCData(const std::string& infoValue, const std::string& infoDCSValue,
+        void addData(const std::string& infoValue, const std::string& infoDCSValue,
          const std::vector<std::string>& infoPinList) {
             value.push_back(infoValue);
-            DCS_value.push_back(infoDCSValue);
+            DCSValue.push_back(infoDCSValue);
             pinlist.push_back(infoPinList);
         }
 
@@ -227,7 +227,7 @@ public:
             }
         }
 
-        void printPSLCData() const {
+        void printData() const {
             std::cout << "PSLC Data:" << std::endl;
 
             std::cout << "Value: ";
@@ -237,7 +237,7 @@ public:
             std::cout << std::endl;
 
             std::cout << "DCSValue: ";
-            for (const auto& value : DCS_value) {
+            for (const auto& value : DCSValue) {
                 std::cout << value << " ";
             }
             std::cout << std::endl;
@@ -245,23 +245,23 @@ public:
         
     };
 
-    std::shared_ptr<PSLC> pslc = std::make_shared<PSLC>();
+    std::shared_ptr<PowerSupplyLoadCapacitance> pslc = std::make_shared<PowerSupplyLoadCapacitance>();
 
-    class PSSL {
+    class PowerSupplySafetyLimits {
     public:
-        std::vector<std::string> min_voltage;
-        std::vector<std::string> max_voltage;
-        std::vector<std::string> max_source_current;
-        std::vector<std::string> max_sink_current;
+        std::vector<std::string> minVoltage;
+        std::vector<std::string> maxVoltage;
+        std::vector<std::string> maxSourceCurrent;
+        std::vector<std::string> maxSinkCurrent;
         std::vector<std::vector<std::string>> pinlist;
 
-        void addPSSLData(const std::string& infoMinVol, const std::string& infoMaxVol,
+        void addData(const std::string& infoMinVol, const std::string& infoMaxVol,
          const std::string& infoMaxSourceCurrent, const std::string& infoMaxSinkCurrent,
           const std::vector<std::string>& infoPinList) {
-            min_voltage.push_back(infoMinVol);
-            max_voltage.push_back(infoMaxVol);
-            max_source_current.push_back(infoMaxSourceCurrent);
-            max_sink_current.push_back(infoMaxSinkCurrent);
+            minVoltage.push_back(infoMinVol);
+            maxVoltage.push_back(infoMaxVol);
+            maxSourceCurrent.push_back(infoMaxSourceCurrent);
+            maxSinkCurrent.push_back(infoMaxSinkCurrent);
             pinlist.push_back(infoPinList);
         }
 
@@ -275,29 +275,29 @@ public:
             }
         }
 
-        void printPSSLData() const {
+        void printData() const {
             std::cout << "PSSL Data:" << std::endl;
 
             std::cout << "MinVoltage: ";
-            for (const auto& vol : min_voltage) {
+            for (const auto& vol : minVoltage) {
                 std::cout << vol << " ";
             }
             std::cout << std::endl;
 
             std::cout << "MaxVoltage: ";
-            for (const auto& vol : max_voltage) {
+            for (const auto& vol : maxVoltage) {
                 std::cout << vol << " ";
             }
             std::cout << std::endl;
 
             std::cout << "MaxSourceCurrent: ";
-            for (const auto& current : max_source_current) {
+            for (const auto& current : maxSourceCurrent) {
                 std::cout << current << " ";
             }
             std::cout << std::endl;
 
             std::cout << "MaxSinkCurrent: ";
-            for (const auto& current : max_sink_current) {
+            for (const auto& current : maxSinkCurrent) {
                 std::cout << current << " ";
             }
             std::cout << std::endl;
@@ -305,7 +305,7 @@ public:
         
     };
 
-    std::shared_ptr<PSSL> pssl = std::make_shared<PSSL>();
+    std::shared_ptr<PowerSupplySafetyLimits> pssl = std::make_shared<PowerSupplySafetyLimits>();
 
 };
 
