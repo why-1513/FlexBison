9489347240e9130bc378f6a1d394e35804acac53
why-1513 <1138828080@qq.com>
2024-01-03 17:26:49 +0800

    增加PSLC类的声明和实现

---
+++ configuration/PowerSupplyLoadCapacitance.cpp
@@ -0,0 +1,35 @@
+#include "PowerSupplyLoadCapacitance.h"
+#include <iostream>
+
+void PowerSupplyLoadCapacitance::addData(const std::string& infoValue, const std::string& infoDCSValue,
+                                         const std::vector<std::string>& infoPinList) {
+    value.push_back(infoValue);
+    DCSValue.push_back(infoDCSValue);
+    pinlist.push_back(infoPinList);
+}
+
+void PowerSupplyLoadCapacitance::printPinList() const {
+    std::cout << "PSLC Pinlist:" << std::endl;
+    for (const auto& pinGroup : pinlist) {
+        for (const auto& pin : pinGroup) {
+            std::cout << pin << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void PowerSupplyLoadCapacitance::printData() const {
+    std::cout << "PSLC Data:" << std::endl;
+
+    std::cout << "Value: ";
+    for (const auto& val : value) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "DCSValue: ";
+    for (const auto& val : DCSValue) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ configuration/PowerSupplyLoadCapacitance.h
@@ -0,0 +1,21 @@
+#ifndef POWERSUPPLYLOADCAPACITANCE_H
+#define POWERSUPPLYLOADCAPACITANCE_H
+
+#include <vector>
+#include <string>
+
+class PowerSupplyLoadCapacitance {
+public:
+    std::vector<std::string> value;
+    std::vector<std::string> DCSValue;
+    std::vector<std::vector<std::string>> pinlist;
+
+    void addData(const std::string& infoValue, const std::string& infoDCSValue,
+                 const std::vector<std::string>& infoPinList);
+
+    void printPinList() const;
+
+    void printData() const;
+};
+
+#endif // POWERSUPPLYLOADCAPACITANCE_H
