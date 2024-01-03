84fe6f43b998d8cbf16a718c68edbc6ed84f4fd8
why-1513 <1138828080@qq.com>
2024-01-03 17:15:26 +0800

    增加DFPS类的声明和实现

---
+++ configuration/DefinePowerSupply.h
@@ -0,0 +1,19 @@
+#ifndef DEFINEPOWERSUPPLY_H
+#define DEFINEPOWERSUPPLY_H
+
+#include <vector>
+#include <string>
+
+class DefinePowerSupply {
+public:
+    std::vector<std::string> dpsChannel;
+    std::vector<std::string> polarity;
+    std::vector<std::string> pin;
+
+    void addData(const std::string& infoDpsChannel,
+                 const std::string& infoPolarity, const std::string& infoPin);
+
+    void printData() const;
+};
+
+#endif // DEFINEPOWERSUPPLY_H
