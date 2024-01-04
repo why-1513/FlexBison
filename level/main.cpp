8b70d262d318dd3b0daef68a9541c22fbed3cf30
why-1513 <1138828080@qq.com>
2024-01-04 11:39:55 +0800

    修改函数命名

--- level/main.cpp
+++ level/main.cpp
@@ -1,25 +1,22 @@
-#include <iostream>
 #include <fstream>
-#include <memory>
 #include "LevelAST.h"
 
 extern int yyparse();
-extern void yyparse_init(const char* filename);
-extern void yyparse_cleanup();
+extern void yyparseInit(const char* filename);
+extern void yyparseCleanup();
 
 int main(int argc, char **argv) {
   
-  const char* level_name;
+  const char* levelName;
   
   std::cout << "Please enter the level file name: ";
   std::string userInput;
   std::cin >> userInput;
 
-  level_name = userInput.c_str();
-  yyparse_init(level_name);
-  // yyparse_init(*(argv+1));
+  levelName = userInput.c_str();
+  yyparseInit(levelName);
   yyparse();
-  yyparse_cleanup();
+  yyparseCleanup();
   
   return 0;
 }
