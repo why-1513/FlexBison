f4dc59f415d08acc47c6b390c09db6189d256728
why-1513 <1138828080@qq.com>
2024-01-05 16:56:35 +0800

    删除主函数创建的logger，通过Logger类来输出

--- configuration/main.cpp
+++ configuration/main.cpp
@@ -1,30 +1,19 @@
 #include <fstream>
 #include "ConfigAST.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
 
 extern int yyparse();
 extern void yyparseInit(const char* filename);
 extern void yyparseCleanup();
 
 int main(int argc, char **argv) {
-  
-  const char* configName;
-  
-  // 创建文件记录器，将日志保存到文件中
-  auto fileLogger = spdlog::basic_logger_mt("ConfigLog", "Config.log");
-  fileLogger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%L] %v");
 
-  // 创建控制台记录器，将日志同时输出到控制台
-  auto consoleLogger = spdlog::stdout_color_mt("ConfigConsoleLog");
-  consoleLogger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%L] %v");
+  const char* configName;
+
+  auto logger = LoggerManager::getLogger();
 
   std::string userInput;
-  consoleLogger->info("Please enter the config file name: ");
-  fileLogger->info("Please enter the config file name: ");
+  logger->info("Please enter the config file name: ");
   std::cin >> userInput;
-  fileLogger->info("Get file input end.");
 
   configName = userInput.c_str();
   yyparseInit(configName);
@@ -44,6 +33,6 @@ int main(int argc, char **argv) {
   configfile->pssl->printPinList();
 
   spdlog::drop_all();
-  
+
   return 0;
 }
