374008a6998c3a132cee2a27ba8369b1b3564ca7
why-1513 <1138828080@qq.com>
2024-01-10 13:16:50 +0800

    修改sink名称

--- configuration/LoggerManager.cpp
+++ configuration/LoggerManager.cpp
@@ -5,13 +5,13 @@ std::shared_ptr<spdlog::logger> LoggerManager::configLogger_ = nullptr;
 std::shared_ptr<spdlog::logger> LoggerManager::getConfigLogger() {
     if (!configLogger_) {
         // 创建文件输出目标
-        auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("Config.log");
+        auto configFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("Config.log");
 
         // 创建控制台输出目标
-        auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        auto configConsoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
         // 创建日志记录器并将输出目标添加到记录器中
-        configLogger_ = std::make_shared<spdlog::logger>("ConfigLogger", spdlog::sinks_init_list({fileSink, consoleSink}));
+        configLogger_ = std::make_shared<spdlog::logger>("ConfigLogger", spdlog::sinks_init_list({configFileSink, configConsoleSink}));
         configLogger_->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%L] %v");
 
     }
