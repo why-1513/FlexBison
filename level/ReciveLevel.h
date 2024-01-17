4a5c5d205a29ec5624f9f0e5b1719e90b6d1d5e6
why-1513 <1138828080@qq.com>
2024-01-17 15:56:58 +0800

    对ReceiveLevel类进行声明

---
+++ level/ReciveLevel.h
@@ -0,0 +1,27 @@
+#ifndef RECIVELEVEL_H
+#define RECIVELEVEL_H
+
+#include <vector>
+#include <string>
+#include "LoggerManager.h"
+
+class ReceiveLevel {
+public:
+    std::vector<std::string> levelSetNo;
+    std::vector<std::string> levelSetId;
+    std::vector<std::string> vth;
+    std::vector<std::string> logicLevel0;
+    std::vector<std::string> logicLevel1;
+
+    std::vector<std::vector<std::string>> pinlist;
+
+    void addData(const std::string infoLevelSetNo, const std::string infoLevelSetId,
+                 const std::string infoVth, const std::string infoLogicLevel0,
+                 const std::string infoLogicLevel1, const std::vector<std::string> infoPinList);
+
+    void printPinList() const;
+
+    void printData() const;
+};
+
+#endif // RECIVELEVEL_H
