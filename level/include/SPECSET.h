76b52757fb4e0a265bfdb146e62522798aa5b438
why-1513 <1138828080@qq.com>
2024-01-31 14:26:14 +0800

    specset部分内容第一版

--- level/include/SPECSET.h
+++ level/include/SPECSET.h
@@ -5,15 +5,22 @@
 #include <string>
 #include "LoggerManager.h"
 
-class SPECSET {
+class SpecSet {
 public:
     std::string specSetNumber;
-    std::string specsetDescription;
+    std::string specSetDescription;
 
     std::vector<std::string> specName;
     std::vector<std::string> setupValue;
     std::vector<std::string> minimumValue;
     std::vector<std::string> maximumValue;
+
+    void setSpecSetNumber(const std::string infoNumber, const std::string infoDescription);
+    void addData(const std::string infoSpecName, const std::string infoSetupValue,
+                 const std::string infoMinimumValue, const std::string infoMaximumValue);
+
+    void printSetNumber() const;
+    void printSetData() const;
   
 };
 
---
+++ level/src/SpecSet.cpp
@@ -0,0 +1,33 @@
+#include "SpecSet.h"
+
+void SpecSet::setSpecSetNumber(const std::string infoNumber, const std::string infoDescription){
+    specSetNumber = infoNumber;
+    specSetDescription = infoDescription;
+}
+
+void SpecSet::addData(const std::string infoSpecName, const std::string infoSetupValue,
+                 const std::string infoMinimumValue, const std::string infoMaximumValue){
+    
+    specName.push_back(infoSpecName);
+    setupValue.push_back(infoSetupValue);
+    minimumValue.push_back(infoMinimumValue);
+    maximumValue.push_back(infoMaximumValue);
+}
+
+void SpecSet::printSetNumber() const {
+    auto levelLogger = LoggerManager::getLevelLogger();
+    levelLogger->info("SpecSet Name: {}", specName);
+    levelLogger->info("SpecSet Description: {}", specSetDescription);
+}
+
+void SpecSet::printSetData() const {
+    auto levelLogger = LoggerManager::getLevelLogger();
+
+    levelLogger->info("SpecSet {} Data:",specName);
+
+    if(!specName.empty()){
+        for(size_t num = 0; num < specName.size(); num++){
+            levelLogger->info("{} {} {} {}", specName[num], setupValue[num],minimumValue[num], maximumValue[num]);
+        }
+    }
+}
