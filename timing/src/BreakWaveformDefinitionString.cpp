c8592b43fd052ca85623456cbd0a34aa26acc04d
why-1513 <1138828080@qq.com>
2024-03-13 16:43:45 +0800

    增加BWDS类

---
+++ timing/src/BreakWaveformDefinitionString.cpp
@@ -0,0 +1,59 @@
+#include "BreakWaveformDefinitionString.h"
+
+void BreakWaveformDefinitionString::addData(const std::string infoWvfSetSet, const std::string infoBreakWaveformIndex,
+                 const std::string infoDefinitionString, const std::vector<std::string> infoPinList){
+    wvfSet.push_back(infoWvfSetSet);
+    breakWaveformIndex.push_back(infoBreakWaveformIndex);
+    definitionString.push_back(infoDefinitionString);
+    pinlist.push_back(infoPinList);
+}
+
+void BreakWaveformDefinitionString::printPinList() const {
+    auto timingLogger = LoggerManager::getTimingLogger();
+    timingLogger->info("BWDS Pinlist:");
+
+    if(!pinlist.empty()){
+        for (const auto pinGroup : pinlist) {
+            std::string vectorContents;
+            for (const auto pin : pinGroup) {
+                vectorContents += pin + " ";
+            }
+            timingLogger->info(vectorContents);
+            vectorContents.clear();
+        }
+    }
+}
+
+void BreakWaveformDefinitionString::printData() const {
+    auto timingLogger = LoggerManager::getTimingLogger();
+    timingLogger->info("BWDS Data:");
+
+    std::string vectorWvfSet;
+    std::string vectorBreakWaveformIndex;
+    std::string vectorDefinitionString;
+
+    if(!wvfSet.empty()){
+        for (const auto set : wvfSet) {
+            vectorWvfSet += set + "";
+        }
+        timingLogger->info("wvfSet: {}", vectorWvfSet);
+        vectorWvfSet.clear();
+    }
+
+    if(!breakWaveformIndex.empty()){
+        for (const auto index : breakWaveformIndex) {
+            vectorBreakWaveformIndex += index + "";
+        }
+        timingLogger->info("breakWaveformIndex: {}", vectorBreakWaveformIndex);
+        vectorBreakWaveformIndex.clear();
+    }
+
+    if(!definitionString.empty()){
+        for (const auto period : definitionString) {
+            vectorDefinitionString += period + "";
+        }
+        timingLogger->info("fixperiod: {}", vectorDefinitionString);
+        vectorDefinitionString.clear();
+    }
+
+}
