63ec895c41805529ed8a6fce85b2013a382ec872
why-1513 <1138828080@qq.com>
2024-03-15 16:56:45 +0800

    修改日志输出

--- timing/src/SpecSet.cpp
+++ timing/src/SpecSet.cpp
@@ -17,19 +17,19 @@ void SpecSet::addData(const std::string infoSpecName, const std::string infoSetu
 }
 
 void SpecSet::printSetNumber() const {
-    auto levelLogger = LoggerManager::getLevelLogger();
-    levelLogger->info("SpecSet Number: {}", specSetNumber);
-    levelLogger->info("SpecSet Description: {}", specSetDescription);
+    auto timingLogger = LoggerManager::getTimingLogger();
+    timingLogger->info("SpecSet Number: {}", specSetNumber);
+    timingLogger->info("SpecSet Description: {}", specSetDescription);
 }
 
 void SpecSet::printSetData() const {
-    auto levelLogger = LoggerManager::getLevelLogger();
+    auto timingLogger = LoggerManager::getTimingLogger();
 
-    levelLogger->info("SpecSet {} Data:",specSetDescription);
+    timingLogger->info("SpecSet {} Data:",specSetDescription);
 
     if(!specNames.empty()){
         for(size_t num = 0; num < specNames.size(); num++){
-            levelLogger->info("{} {} {} {} {}", specNames[num], setupValues[num],minimumValues[num], maximumValues[num], units[num]);
+            timingLogger->info("{} {} {} {} {}", specNames[num], setupValues[num],minimumValues[num], maximumValues[num], units[num]);
         }
     }
 }
