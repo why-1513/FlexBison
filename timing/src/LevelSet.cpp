a0de62b1a4647cc9e21a401be6ab1886540b8662
why-1513 <1138828080@qq.com>
2024-03-19 16:09:43 +0800

    增加编译cpp文件

---
+++ timing/src/LevelSet.cpp
@@ -0,0 +1,41 @@
+#include "LevelSet.h"
+
+void LevelSet::setNumber(const std::string infoNumber, const std::string infoDescription){
+    levelSetNumber = infoNumber;
+    levelSetDescription = infoDescription;
+}
+
+void LevelSet::addPinsInData(const std::string infoPinsInName, const std::string infoPinsInValue){
+    pinsInNames.push_back(infoPinsInName);
+    pinsInValues.push_back(infoPinsInValue);
+}
+
+void LevelSet::addPinsOutData(const std::string infoPinsOutName, const std::string infoPinsOutValue){
+    pinsOutNames.push_back(infoPinsOutName);
+    pinsOutValues.push_back(infoPinsOutValue);
+}
+
+void LevelSet::printSetNumber() const {
+    auto levelLogger = LoggerManager::getTimingLogger();
+    levelLogger->info("levelSet Number: {}", levelSetNumber);
+    levelLogger->info("levelSet Description: {}", levelSetDescription);
+}
+
+void LevelSet::printSetData() const {
+    auto levelLogger = LoggerManager::getTimingLogger();
+
+    if(!pinsInNames.empty()){
+        levelLogger->info("LevelSet Pins In Data:");
+        for(size_t num = 0; num < pinsInNames.size(); num++){
+            levelLogger->info("{} = {}", pinsInNames[num], pinsInValues[num]);
+        }
+    }
+
+    if(!pinsOutNames.empty()){
+        levelLogger->info("LevelSet Pins Out Data:");
+        for(size_t num = 0; num < pinsOutNames.size(); num++){
+            levelLogger->info("{} = {}", pinsOutNames[num], pinsOutValues[num]);
+        }
+    }
+
+}
